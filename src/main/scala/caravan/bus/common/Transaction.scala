39267823c71a2a2fd9d4d7e3376d72986b7caedb
Hadir Khan <hadirkhan10@gmail.com>
2021-03-06 18:31:28 +0500

    Added a transaction base class which provides a standard template for the core to talk to the bus adapters

---
+++ src/main/scala/caravan/bus/common/Transaction.scala
@@ -0,0 +1,17 @@
+package caravan.bus.common
+import chisel3._
+
+/**
+ * This abstract class provides a template for other protocols to implement the transaction wires.
+ * This is used as a template for e.g when the core wants to communicate with the memory or with the peripheral registers.
+ * It will set these signals up in order to talk to the Host adapter of the relevant bus protocol
+ */
+abstract class Transaction extends Bundle {
+  val validRequest: Bool
+  val addrRequest: UInt
+  val dataRequest: UInt
+  val transactionSize: UInt
+  val isWrite: Bool
+  val validResponse: Bool
+  val dataResponse: UInt
+}
