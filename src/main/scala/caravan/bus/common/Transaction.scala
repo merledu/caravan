464c4e729e825b6383e935186e3d1b1ebb193133
Hadir Khan <hadirkhan10@gmail.com>
2021-03-11 23:42:43 +0500

    Created two bundles FromIp and FromBus to better indicate the communication bundles

--- src/main/scala/caravan/bus/common/Transaction.scala
+++ src/main/scala/caravan/bus/common/Transaction.scala
@@ -6,12 +6,17 @@ import chisel3._
  * This is used as a template for e.g when the core wants to communicate with the memory or with the peripheral registers.
  * It will set these signals up in order to talk to the Host adapter of the relevant bus protocol
  */
-abstract class Transaction extends Bundle {
-  val validRequest: Bool
+abstract class FromIp extends Bundle {
+//  val validRequest: Bool
   val addrRequest: UInt
   val dataRequest: UInt
   val activeByteLane: UInt
   val isWrite: Bool
-  val validResponse: Bool
+//  val validResponse: Bool
+//  val dataResponse: UInt
+}
+
+abstract class FromBus extends Bundle {
   val dataResponse: UInt
+  val ackWrite: Bool
 }
