b5d1b3e3d7a0d59e1b3c7d5e4f23bdac8a137175
Hadir Khan <hadirkhan10@gmail.com>
2021-04-18 16:07:20 +0500

    modified for proper connectivity, testing remains to be done

--- src/main/scala/caravan/bus/common/Switch1toN.scala
+++ src/main/scala/caravan/bus/common/Switch1toN.scala
@@ -8,8 +8,8 @@ class Switch1toN[A <: BusHost, B <: BusDevice](mb: A, sb: B, N: Int) extends Mod
   val io = IO(new Bundle {
     val hostIn = Flipped(Decoupled(mb))
     val hostOut = Decoupled(sb)
-    val devOut = Vec(N, Decoupled(mb))
-    val devIn = Flipped(Vec(N, Decoupled(sb)))
+    val devOut = Vec(N+1, Decoupled(mb))  // creating 1 extra to connect error device
+    val devIn = Flipped(Vec(N+1, Decoupled(sb)))  // creating 1 extra to connect error device
     val devSel = Input(UInt(log2Ceil(N + 1).W))
   })
 
@@ -18,20 +18,31 @@ class Switch1toN[A <: BusHost, B <: BusDevice](mb: A, sb: B, N: Int) extends Mod
   /** FIXME: assuming the socket is always ready to accept data from all the devices */
   io.devIn.map(b => b.ready := true.B)
 
+  /** sending valid to error device only when host sends a valid req and the decoder cannot match
+   * any address with the address map and sends a devSel for error device */
+  io.devOut(N).valid := io.hostIn.valid && (io.devSel === N.asUInt)
+  /** connecting the response with the error device by default
+   * this would be overridden below if devSel matches with any devices */
+  io.hostOut.valid := io.devIn(N).valid
+  io.hostOut.bits <> io.devIn(N).bits
 
+  /** connecting the bits from the host to all the devices
+   * but connecting the valid to only that device which is selected by the decoder */
+  io.devOut.map(dev => dev.bits <> io.hostIn.bits)
   for (i <- 0 until N) {
-    when(io.devSel === i.asUInt()) {
-      io.hostOut <> io.devIn(i)
-    }.otherwise {
-      io.hostOut.valid := false.B
-      io.hostOut.bits := DontCare
+    io.devOut(i).valid := io.hostIn.valid && (io.devSel === i.asUInt)
+  }
+
+  /** if the devSel matches, then wire the host out with that device's signals
+   * else, keep them connected with the error responder device. */
+  for (id <- 0 until N) {
+    when(io.devSel === id.asUInt) {
+      io.hostOut.bits <> io.devIn(id).bits
+      io.hostOut.valid := io.devIn(id).valid
     }
   }
 
-  for (i <- 0 until N) {
-    io.devOut(i) <> io.hostIn
-    io.devOut(i).valid := io.hostIn.valid && (io.devSel === i.asUInt())
-  }
+
 }
 
 
