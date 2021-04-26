3c642c7d66f80e49c846cd1807dc4d15ea7f987c
Hadir Khan <hadirkhan10@gmail.com>
2021-04-26 16:35:50 +0500

    modified the bus decoder to correctly decode the id given the address

--- src/main/scala/caravan/bus/common/BusDecoder.scala
+++ src/main/scala/caravan/bus/common/BusDecoder.scala
@@ -1,25 +1,26 @@
 package caravan.bus.common
 import caravan.bus.wishbone.Peripherals
-import caravan.bus.wishbone.Peripherals.Peripheral
+import caravan.bus.wishbone.Peripherals._
 
 import scala.collection._
 import chisel3._
+import chisel3.util.{MuxCase, log2Ceil}
 
 /** creating a type inside object so that it can be easily imported
  * BusMap type provides a short way of mentioning mutable.Map[Peripheral, (UInt, UInt)] everywhere */
 object BusMap {
-  type BusMap = mutable.Map[Peripheral, (UInt, UInt)]
+  type BusMap = mutable.Map[Peripherals.Type, (UInt, UInt)]
 }
 
 import BusMap._
 /** This class provides the user an API to define peripherals and their address mapping */
 class AddressMap {
-  private val map: BusMap = mutable.Map[Peripheral, (UInt, UInt)]()
+  private val map: BusMap = mutable.Map[Peripherals.Type, (UInt, UInt)]()
   /** addDevice provides the user to add each device to the address map */
   /** FIXME: there is no restriction on adding two peripherals with same base address.
    * logically this should never happen, however user can add two peripherals with same base addresses
    * there is no check for this and would break the code in later steps when we decode the addr of a peripheral */
-  def addDevice(peripheral: Peripheral, baseAddr: UInt, addrMask: UInt): Unit = map += (peripheral -> (baseAddr, addrMask))
+  def addDevice(peripheral: Peripherals.Type, baseAddr: UInt, addrMask: UInt): Unit = map += (peripheral -> (baseAddr, addrMask))
   /** an helper function that returns the map [Encapsulation] */
   def getMap(): BusMap = map
 }
@@ -27,21 +28,36 @@ class AddressMap {
 /** BusDecoder provides an helpful utility to decode the address and send a device sel to the bus switch
  *  with which different peripherals are connected */
 object BusDecoder {
-  /** decode takes the addr from the host and an address map and figures out which peripheral's id should be sent as dev sel to the switch */
+  /** decode takes the addr from the host and an address map and figures out which peripheral's id should be sent
+   *  as dev sel to the switch */
   def decode(addr: UInt, addressMap: AddressMap): UInt = {
-    val matchingPeripheralList = addressMap.getMap().filter(d => filterAddr(d, addr)).toList
-    /** assuming that AddressMap always has different addresses of peripherals.
-     * so the filter would always return a Map with one matching peripheral that is converted into List
-     * or an empty Map if the address did not match any peripheral and converted into an empty List
-     * so we are checking if the List is empty, then send a dev sel equal to the size of map which would always be
-     * one more than the peripherals in the map. This way the switch can see there is an error and return an error response
-     * else it extracts the first (only) peripheral and finds its id and sends it to the switch */
-    if (matchingPeripheralList.isEmpty)
-      addressMap.getMap().size.asUInt
-    else
-      matchingPeripheralList.head._1.id.asUInt
+    // contains which peripheral's addr hit
+    val addr_hit = Wire(Vec(addressMap.getMap().size, Bool()))
+    // contains the id of the matched peripheral
+    val id = Wire(Vec(addressMap.getMap().size, UInt(log2Ceil(addressMap.getMap().size).W)))
+
+    /** looping over the map to:
+     * 1) assert the appropriate wire of the peripheral with whom the received address matches
+     * 2) store the id of that peripheral */
+    for (i <- 0 until addressMap.getMap().size) {
+      addr_hit(i) := (addressMap.getMap().toList(i)._2._2 & addr) === addressMap.getMap().toList(i)._2._1
+      id(i) := Mux((addressMap.getMap().toList(i)._2._2 & addr) === addressMap.getMap().toList(i)._2._1,  // condition
+                    addressMap.getMap.toList(i)._1.asUInt,  // if true
+                    addressMap.getMap().size.U)             // if false send the id as the total size which has err module connected
+    }
+
+    /** creating cases for the MuxCase construct.
+     * addr_hit zip id -> something like this:
+     * ((true.B, 1.U), (false.B, 2.U))
+     * returning a sequence for MuxCase
+     * where if bool is true the id it contains will be forwarded
+     * else by defualt size will be forwarded
+     * with which the error module will be connected */
+
+    val cases = addr_hit zip id map { case (bool, id) =>
+      bool -> id
+    }
+
+    MuxCase(addressMap.getMap().size.U, cases)
   }
-  /** helper functions used by the decode function above
-   * selects the peripheral based on the address received */
-  def filterAddr(map: (Peripheral, (UInt, UInt)), addr: UInt): Boolean = ((addr & map._2._2) === map._2._1).asInstanceOf[Boolean]
 }
