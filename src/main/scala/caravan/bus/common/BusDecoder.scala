31776edb6384b7977d8e1f1905f1b94c6b1b08cb
Hadir Khan <hadirkhan10@gmail.com>
2021-04-28 21:41:38 +0500

    changed the type of the bus map to also manage the devices

--- src/main/scala/caravan/bus/common/BusDecoder.scala
+++ src/main/scala/caravan/bus/common/BusDecoder.scala
@@ -1,5 +1,5 @@
 package caravan.bus.common
-import caravan.bus.wishbone.Peripherals
+import caravan.bus.wishbone.{Peripherals, WishboneDevice}
 import caravan.bus.wishbone.Peripherals._
 
 import scala.collection._
@@ -9,20 +9,23 @@ import chisel3.util.{MuxCase, log2Ceil}
 /** creating a type inside object so that it can be easily imported
  * BusMap type provides a short way of mentioning mutable.Map[Peripheral, (UInt, UInt)] everywhere */
 object BusMap {
-  type BusMap = mutable.Map[Peripherals.Type, (UInt, UInt)]
+  type BusMap = mutable.Map[Peripherals.Type, (UInt, UInt, WishboneDevice)]
 }
 
 import BusMap._
 /** This class provides the user an API to define peripherals and their address mapping */
 class AddressMap {
-  private val map: BusMap = mutable.Map[Peripherals.Type, (UInt, UInt)]()
+  private val map: BusMap = mutable.Map[Peripherals.Type, (UInt, UInt, WishboneDevice)]()
   /** addDevice provides the user to add each device to the address map */
   /** FIXME: there is no restriction on adding two peripherals with same base address.
    * logically this should never happen, however user can add two peripherals with same base addresses
    * there is no check for this and would break the code in later steps when we decode the addr of a peripheral */
-  def addDevice(peripheral: Peripherals.Type, baseAddr: UInt, addrMask: UInt): Unit = map += (peripheral -> (baseAddr, addrMask))
+  def addDevice(peripheral: Peripherals.Type, baseAddr: UInt, addrMask: UInt, wbDevice: WishboneDevice): Unit = map += (peripheral -> (baseAddr, addrMask, wbDevice))
   /** an helper function that returns the map [Encapsulation] */
   def getMap(): BusMap = map
+  def getDevices: Seq[(WishboneDevice, Peripherals.Type)] = {
+    getMap().map(f => (f._2._3, f._1)).toSeq
+  }
 }
 
 /** BusDecoder provides an helpful utility to decode the address and send a device sel to the bus switch
