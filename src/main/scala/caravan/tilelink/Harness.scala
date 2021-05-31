1a1fd97e70bed5d46e51f103052ae069f93a90b0
shahzaib <shahzaibceo@gmail.com>
2021-06-01 00:29:38 +0500

    Masked Memory Added in Harness

--- src/main/scala/caravan/tilelink/Harness.scala
+++ src/main/scala/caravan/tilelink/Harness.scala
@@ -3,7 +3,7 @@ import caravan.bus.common.{AddressMap, BusDecoder, DeviceAdapter, Switch1toN}
 import chisel3._
 import chisel3.experimental.ChiselEnum
 import chisel3.stage.ChiselStage
-import chisel3.util.{Cat, Decoupled}
+import chisel3.util.{Cat, Decoupled, MuxLookup}
 import chisel3.util.experimental.loadMemoryFromFile
 
 
@@ -13,16 +13,27 @@ class DummyMemController(programFile: Option[String])(implicit val config: Tilel
         val rsp = Decoupled(new TLResponse())
     })
 
-    val mem = SyncReadMem(1024, UInt(32.W))
+    val mem = SyncReadMem(1024, UInt((config.w*8).W))
+
+    
 
     when(io.req.bits.isWrite){
         mem.write(io.req.bits.addrRequest, io.req.bits.dataRequest)
         io.rsp.bits.dataResponse := io.req.bits.dataRequest
         io.rsp.bits.error := false.B
     }.otherwise{
-        io.rsp.bits.dataResponse := mem.read(io.req.bits.addrRequest)
+        io.rsp.bits.dataResponse := MuxLookup(io.req.bits.activeByteLane, 
+        mem.read(io.req.bits.addrRequest),
+        Array(
+          ("b0001".U) -> Cat(0.U(24.W), mem.read(io.req.bits.addrRequest)(7,0)),
+          ("b0011".U) -> Cat(0.U(16.W), mem.read(io.req.bits.addrRequest)(15,0)),
+          ("b0111".U) -> Cat(0.U(8.W), mem.read(io.req.bits.addrRequest)(23,0)),
+          ("b1111".U) -> mem.read(io.req.bits.addrRequest),
+        ))
+        // io.rsp.bits.dataResponse := mem.read(io.req.bits.addrRequest, io.req.valid)
         io.rsp.bits.error := false.B
     }
+
     io.rsp.valid := true.B
     io.req.ready := true.B
 
