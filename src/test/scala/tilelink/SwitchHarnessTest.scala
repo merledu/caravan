68d89aaefbb984fcf752cb80a9de02574ab7a19b
shahzaib <shahzaibceo@gmail.com>
2021-06-11 10:22:11 +0500

    Switch Harness 1:N Tested ✅

--- src/test/scala/tilelink/SwitchHarnessTest.scala
+++ src/test/scala/tilelink/SwitchHarnessTest.scala
@@ -52,6 +52,7 @@ class SwitchHarnessTest extends FreeSpec with ChiselScalatestTester with MemoryD
       }
       println("VALID RESPONSE = " + c.io.validResp.peek().litToBoolean.toString)
       println("Got the response now reading expected data")
+      c.clock.step(1)
       c.io.dataResp.expect(1.U)
       println("EXPECTED DATA IS: 1 GOT " + c.io.dataResp.peek().litValue().toInt.toString)
       c.clock.step(2)
@@ -63,6 +64,7 @@ class SwitchHarnessTest extends FreeSpec with ChiselScalatestTester with MemoryD
       }
       println("VALID RESPONSE = " + c.io.validResp.peek().litToBoolean.toString)
       println("Got the response now reading expected data")
+      c.clock.step(1)
       c.io.dataResp.expect(2.U)
       println("EXPECTED DATA IS: 2 GOT " + c.io.dataResp.peek().litValue().toInt.toString)
       c.clock.step(2)
@@ -74,6 +76,7 @@ class SwitchHarnessTest extends FreeSpec with ChiselScalatestTester with MemoryD
       }
       println("VALID RESPONSE = " + c.io.validResp.peek().litToBoolean.toString)
       println("Got the response now reading expected data")
+      c.clock.step(1)
       c.io.dataResp.expect(3.U)
       println("EXPECTED DATA IS: 3 GOT " + c.io.dataResp.peek().litValue().toInt.toString)
       c.clock.step(10)
@@ -103,6 +106,7 @@ class SwitchHarnessTest extends FreeSpec with ChiselScalatestTester with MemoryD
       }
       println("VALID RESPONSE = " + c.io.validResp.peek().litToBoolean.toString)
       println("Got the response now reading expected error")
+      c.clock.step(1)
       c.io.errResp.expect(true.B)
       println("EXPECTED ERR IS: true GOT " + c.io.errResp.peek().litToBoolean.toString)
       c.clock.step(10)
@@ -120,83 +124,86 @@ class SwitchHarnessTest extends FreeSpec with ChiselScalatestTester with MemoryD
     }
   }
 
-  // "should write data to multiple rows and read them back from memory" in {
-  //   implicit val config = WishboneConfig(32, 32)
-  //   val programFile = getFile
-  //   test(new SwitchHarness(programFile)).withAnnotations(Seq(VerilatorBackendAnnotation)) {c =>
-  //     c.clock.step(5)
-  //     sendRequestToMem("h40000000".U, "h00100120".U, "b1111".U, true.B)
-  //     println("VALID RESPONSE = " + c.io.validResp.peek().litToBoolean.toString)
-  //     while(c.io.validResp.peek().litToBoolean != true) {
-  //       println("wait")
-  //       c.clock.step(1)
-  //     }
-  //     println("VALID RESPONSE = " + c.io.validResp.peek().litToBoolean.toString)
-  //     println("Got the response now sending new request")
-  //     c.clock.step(2)
-  //     sendRequestToMem("h40000004".U, "h00100124".U, "b1111".U, true.B)
-  //     println("VALID RESPONSE = " + c.io.validResp.peek().litToBoolean.toString)
-  //     while(c.io.validResp.peek().litToBoolean != true) {
-  //       println("wait")
-  //       c.clock.step(1)
-  //     }
-  //     println("VALID RESPONSE = " + c.io.validResp.peek().litToBoolean.toString)
-  //     println("Got the response now sending new request")
-  //     c.clock.step(2)
-  //     sendRequestToMem("h40000008".U, "h00100128".U, "b1111".U, true.B)
-  //     println("VALID RESPONSE = " + c.io.validResp.peek().litToBoolean.toString)
-  //     while(c.io.validResp.peek().litToBoolean != true) {
-  //       println("wait")
-  //       c.clock.step(1)
-  //     }
-  //     println("VALID RESPONSE = " + c.io.validResp.peek().litToBoolean.toString)
-  //     println("Got the response now sending new request")
-  //     sendRequestToMem("h40000000".U, 0.U, "b1111".U, false.B)
-  //     println("VALID RESPONSE = " + c.io.validResp.peek().litToBoolean.toString)
-  //     while(c.io.validResp.peek().litToBoolean != true) {
-  //       println("wait")
-  //       c.clock.step(1)
-  //     }
-  //     println("VALID RESPONSE = " + c.io.validResp.peek().litToBoolean.toString)
-  //     println("Got the response now reading expected data")
-  //     c.io.dataResp.expect("h00100120".U)
-  //     println("EXPECTED DATA IS: " + "h00100120".U.litValue().toInt.toString + " GOT " + c.io.dataResp.peek().litValue().toInt.toString)
-  //     c.clock.step(2)
-  //     sendRequestToMem("h40000004".U, 0.U, "b1111".U, false.B)
-  //     println("VALID RESPONSE = " + c.io.validResp.peek().litToBoolean.toString)
-  //     while(c.io.validResp.peek().litToBoolean != true) {
-  //       println("wait")
-  //       c.clock.step(1)
-  //     }
-  //     println("VALID RESPONSE = " + c.io.validResp.peek().litToBoolean.toString)
-  //     println("Got the response now reading expected data")
-  //     c.io.dataResp.expect("h00100124".U)
-  //     println("EXPECTED DATA IS: " + "h00100124".U.litValue().toInt.toString + " GOT " + c.io.dataResp.peek().litValue().toInt.toString)
-  //     c.clock.step(2)
-  //     sendRequestToMem("h40000008".U, 0.U, "b1111".U, false.B)
-  //     println("VALID RESPONSE = " + c.io.validResp.peek().litToBoolean.toString)
-  //     while(c.io.validResp.peek().litToBoolean != true) {
-  //       println("wait")
-  //       c.clock.step(1)
-  //     }
-  //     println("VALID RESPONSE = " + c.io.validResp.peek().litToBoolean.toString)
-  //     println("Got the response now reading expected data")
-  //     c.io.dataResp.expect("h00100128".U)
-  //     println("EXPECTED DATA IS: " + "h00100128".U.litValue().toInt.toString + " GOT " + c.io.dataResp.peek().litValue().toInt.toString)
-  //     c.clock.step(10)
+  "should write data to multiple rows and read them back from memory" in {
+    implicit val config = TilelinkConfig()
+    // val programFile = getFile
+    test(new SwitchHarness()).withAnnotations(Seq(VerilatorBackendAnnotation)) {c =>
+      c.clock.step(5)
+      sendRequestToMem("h40000000".U, "h00100120".U, "b1111".U, true.B)
+      println("VALID RESPONSE = " + c.io.validResp.peek().litToBoolean.toString)
+      while(c.io.validResp.peek().litToBoolean != true) {
+        println("wait")
+        c.clock.step(1)
+      }
+      println("VALID RESPONSE = " + c.io.validResp.peek().litToBoolean.toString)
+      println("Got the response now sending new request")
+      c.clock.step(2)
+      sendRequestToMem("h40000004".U, "h00100124".U, "b1111".U, true.B)
+      println("VALID RESPONSE = " + c.io.validResp.peek().litToBoolean.toString)
+      while(c.io.validResp.peek().litToBoolean != true) {
+        println("wait")
+        c.clock.step(1)
+      }
+      println("VALID RESPONSE = " + c.io.validResp.peek().litToBoolean.toString)
+      println("Got the response now sending new request")
+      c.clock.step(2)
+      sendRequestToMem("h40000008".U, "h00100128".U, "b1111".U, true.B)
+      println("VALID RESPONSE = " + c.io.validResp.peek().litToBoolean.toString)
+      while(c.io.validResp.peek().litToBoolean != true) {
+        println("wait")
+        c.clock.step(1)
+      }
+      println("VALID RESPONSE = " + c.io.validResp.peek().litToBoolean.toString)
+      println("Got the response now sending new request")
+      sendRequestToMem("h40000000".U, 0.U, "b1111".U, false.B)
+      println("VALID RESPONSE = " + c.io.validResp.peek().litToBoolean.toString)
+      while(c.io.validResp.peek().litToBoolean != true) {
+        println("wait")
+        c.clock.step(1)
+      }
+      println("VALID RESPONSE = " + c.io.validResp.peek().litToBoolean.toString)
+      println("Got the response now reading expected data")
+      c.clock.step(1)
+      c.io.dataResp.expect("h00100120".U)
+      println("EXPECTED DATA IS: " + "h00100120".U.litValue().toInt.toString + " GOT " + c.io.dataResp.peek().litValue().toInt.toString)
+      c.clock.step(2)
+      sendRequestToMem("h40000004".U, 0.U, "b1111".U, false.B)
+      println("VALID RESPONSE = " + c.io.validResp.peek().litToBoolean.toString)
+      while(c.io.validResp.peek().litToBoolean != true) {
+        println("wait")
+        c.clock.step(1)
+      }
+      println("VALID RESPONSE = " + c.io.validResp.peek().litToBoolean.toString)
+      println("Got the response now reading expected data")
+      c.clock.step(1)
+      c.io.dataResp.expect("h00100124".U)
+      println("EXPECTED DATA IS: " + "h00100124".U.litValue().toInt.toString + " GOT " + c.io.dataResp.peek().litValue().toInt.toString)
+      c.clock.step(2)
+      sendRequestToMem("h40000008".U, 0.U, "b1111".U, false.B)
+      println("VALID RESPONSE = " + c.io.validResp.peek().litToBoolean.toString)
+      while(c.io.validResp.peek().litToBoolean != true) {
+        println("wait")
+        c.clock.step(1)
+      }
+      println("VALID RESPONSE = " + c.io.validResp.peek().litToBoolean.toString)
+      println("Got the response now reading expected data")
+      c.clock.step(1)
+      c.io.dataResp.expect("h00100128".U)
+      println("EXPECTED DATA IS: " + "h00100128".U.litValue().toInt.toString + " GOT " + c.io.dataResp.peek().litValue().toInt.toString)
+      c.clock.step(10)
 
-  //     def sendRequestToMem(addr: UInt, data: UInt, byteLane: UInt, isWrite: Bool): Unit = {
-  //       c.clock.step(1)
-  //       c.io.valid.poke(true.B)
-  //       c.io.addrReq.poke(addr)
-  //       c.io.dataReq.poke(data)
-  //       c.io.byteLane.poke(byteLane)
-  //       c.io.isWrite.poke(isWrite)
-  //       c.clock.step(1)
-  //       c.io.valid.poke(false.B)
-  //     }
-  //   }
-  // }
+      def sendRequestToMem(addr: UInt, data: UInt, byteLane: UInt, isWrite: Bool): Unit = {
+        c.clock.step(1)
+        c.io.valid.poke(true.B)
+        c.io.addrReq.poke(addr)
+        c.io.dataReq.poke(data)
+        c.io.byteLane.poke(byteLane)
+        c.io.isWrite.poke(isWrite)
+        c.clock.step(1)
+        c.io.valid.poke(false.B)
+      }
+    }
+  }
 
   "should write to a device that is not in memory map and produce error" in {
     implicit val config = TilelinkConfig()
@@ -210,6 +217,7 @@ class SwitchHarnessTest extends FreeSpec with ChiselScalatestTester with MemoryD
       }
       println("VALID RESPONSE = " + c.io.validResp.peek().litToBoolean.toString)
       println("Got the response now reading expected error")
+      c.clock.step(1)
       c.io.errResp.expect(true.B)
       println("EXPECTED ERR IS: true GOT " + c.io.errResp.peek().litToBoolean.toString)
       c.clock.step(10)
