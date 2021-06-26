bc423025222f55c0b7353dba819e8e8fddab2caf
shahzaib <shahzaibceo@gmail.com>
2021-06-27 03:40:08 +0500

    Device accepting dynamic values

--- src/main/scala/caravan/bus/tilelink/TilelinkDevice.scala
+++ src/main/scala/caravan/bus/tilelink/TilelinkDevice.scala
@@ -2,7 +2,7 @@ package caravan.bus.tilelink
 import caravan.bus.common.DeviceAdapter
 import chisel3._
 import chisel3.stage.ChiselStage
-import chisel3.util.Decoupled
+import chisel3.util.{Decoupled, MuxLookup}
 
 class TilelinkDevice(implicit val config: TilelinkConfig) extends DeviceAdapter with OpCodes {
     val io = IO(new Bundle {
@@ -20,18 +20,19 @@ class TilelinkDevice(implicit val config: TilelinkConfig) extends DeviceAdapter 
 
 
     // Sending Response coming from Memory in the STALL to delay the response one cycle
-    stall.io.bundle_in.d_opcode := Mux(io.tlMasterReceiver.bits.a_opcode === Get.U, AccessAckData.U, AccessAck.U)
+    stall.io.bundle_in.d_opcode := Mux(io.rspIn.bits.error, 2.U, Mux(io.tlMasterReceiver.bits.a_opcode === Get.U, AccessAckData.U, Mux(io.tlMasterReceiver.bits.a_opcode === PutFullData.U || io.tlMasterReceiver.bits.a_opcode === PutPartialData.U, AccessAck.U, 2.U)))
     stall.io.bundle_in.d_data := io.rspIn.bits.dataResponse
     stall.io.bundle_in.d_param := 0.U
     stall.io.bundle_in.d_size := io.tlMasterReceiver.bits.a_size
     stall.io.bundle_in.d_source := io.tlMasterReceiver.bits.a_source
     stall.io.bundle_in.d_sink := 0.U
-    stall.io.bundle_in.d_denied := false.B
-    stall.io.bundle_in.d_corrupt := io.rspIn.bits.error
-    stall.io.valid_in := true.B
+    stall.io.bundle_in.d_denied := io.rspIn.bits.error
+    stall.io.bundle_in.d_corrupt := 0.U
+    stall.io.valid_in := io.rspIn.valid
 
     io.tlSlaveTransmitter.bits := stall.io.bundle_out
     io.tlSlaveTransmitter.valid := stall.io.valid_out
+
 
     io.reqOut.bits.addrRequest := io.tlMasterReceiver.bits.a_address
     io.reqOut.bits.dataRequest := io.tlMasterReceiver.bits.a_data
