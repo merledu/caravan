e6d782e2556f2825da31fe39d5e6dad82c040df1
shahzaib <shahzaibceo@gmail.com>
2021-06-27 04:20:30 +0500

    removed PeripheralMap from TL

--- src/main/scala/caravan/bus/tilelink/PeripheralMap.scala
+++
@@ -1,8 +0,0 @@
-// package caravan.bus.tilelink
-// import chisel3._
-// import chisel3.experimental.ChiselEnum
-
-// object Peripherals extends ChiselEnum {
-//   val GPIO = Value(0.U)
-//   val DCCM = Value(1.U)
-// }
